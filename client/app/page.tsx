eaee59efbfaa22170da89cb4aaa2b0056b8beb34
dasd171 <85402728+dasd171@users.noreply.github.com>
2026-05-13 15:45:45 +0800

    Update page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -60,20 +60,28 @@ export default function Home() {
 
   function createPeer(remoteId: string) {
     const peer = new RTCPeerConnection({
-     iceServers: [
-  { urls: "stun:stun.l.google.com:19302" },
-  {
-    urls: "turn:openrelay.metered.ca:80",
-    username: "openrelayproject",
-    credential: "openrelayproject"
-  },
-  {
-    urls: "turn:openrelay.metered.ca:443",
-    username: "openrelayproject",
-    credential: "openrelayproject"
-  }
-]
-    });
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    },
+
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    },
+
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    }
+  ]
+});
 
     peer.onicecandidate = (e) => {
       if (e.candidate) {
